5ac65dc19a74c9f9fccbbaf2d7b5df7b12b545f0
LebedenkoAndrii <lebedenko1928@gmail.com>
2026-01-05 20:48:18 +0200

    add populate for all data of deputy

--- src/api/deputy/controllers/deputy.ts
+++ src/api/deputy/controllers/deputy.ts
@@ -2,6 +2,21 @@
  * deputy controller
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::deputy.deputy');
+export default factories.createCoreController(
+  "api::deputy.deputy",
+  ({ strapi }) => ({
+    async find(ctx) {
+      ctx.query = { ...ctx.query, populate: "*" };
+      const { data, meta } = await super.find(ctx);
+      return { data, meta };
+    },
+
+    async findOne(ctx) {
+      ctx.query = { ...ctx.query, populate: "*" };
+      const { data, meta } = await super.findOne(ctx);
+      return { data, meta };
+    },
+  })
+);
