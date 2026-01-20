8c673547e1d82489d8490a1957e717e0cf5b2171
LebedenkoAndrii <lebedenko1928@gmail.com>
2026-01-20 21:15:30 +0200

    remove unnecessarry code of find and findOne in deputy controller

--- src/api/deputy/controllers/deputy.ts
+++ src/api/deputy/controllers/deputy.ts
@@ -2,21 +2,6 @@
  * deputy controller
  */
 
-import { factories } from "@strapi/strapi";
+import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController(
-  "api::deputy.deputy",
-  ({ strapi }) => ({
-    async find(ctx) {
-      ctx.query = { ...ctx.query, populate: "*" };
-      const { data, meta } = await super.find(ctx);
-      return { data, meta };
-    },
-
-    async findOne(ctx) {
-      ctx.query = { ...ctx.query, populate: "*" };
-      const { data, meta } = await super.findOne(ctx);
-      return { data, meta };
-    },
-  })
-);
+export default factories.createCoreController('api::deputy.deputy');
