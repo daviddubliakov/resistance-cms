1c9e6682dd08c2f9fb408d8025c56cbe366d4958
LebedenkoAndrii <lebedenko1928@gmail.com>
2026-02-08 21:10:20 +0200

    add sort of deputies and unshift mayor to first place in array

--- src/api/deputy/controllers/deputy.ts
+++ src/api/deputy/controllers/deputy.ts
@@ -4,4 +4,26 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::deputy.deputy');
+export default factories.createCoreController(
+  'api::deputy.deputy',
+  ({ strapi }) => ({
+    async find(ctx) {
+      ctx.query = { ...ctx.query, sort: 'lastName:asc' };
+
+      const { data, meta } = await super.find(ctx);
+      if (!data || data.length === 0) return { data, meta };
+
+      const mayorIndex = data.findIndex((deputy) => {
+        const employment = deputy.attributes.placeOfEmployment || '';
+        return employment.includes('Міський голова');
+      });
+
+      if (mayorIndex !== -1) {
+        const [mayor] = data.splice(mayorIndex, 1);
+        data.unshift(mayor);
+      }
+
+      return { data, meta };
+    },
+  }),
+);
